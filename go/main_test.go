99c367581d661f3e9dc51de6b778352380015a6c
Vladimir Kornilov <kornilov@kolesa.kz>
2022-11-09 17:56:13 +0600

    Написал тесты на go

--- go/main_test.go
+++ go/main_test.go
@@ -23,6 +23,22 @@ import (
 // TitleCase("the quick fox in the bag", "") = "The Quick Fox In The Bag"
 // TitleCase("the quick fox in the bag", "in the") = "The Quick Fox in the Bag"
 
+var DataProvider = []struct {
+	Str, Minor, Want string
+}{
+	{"", "", ""},
+	{"test text", "", "Test Text"},
+	{"test text", "test", "Test Text"},
+	{"", "test", ""},                        //first empty
+	{"test text", "test text", "Test text"}, //same text as first and second args
+	{"1 2 3", "2", "1 2 3"},                 //numbers
+	{"! @ #", "@", "! @ #"},                 //symbols
+	{"test this simple text", "text this simple", "Test this simple text"}, //disordered words
+	{"test text", "AAAAA", "Test Text"},                                    //second arg isn't substring
+	{"test text", "TEST TEXT", "Test Text"},                                //second arg is case-sensitive
+	{"TEST UPPERCASE uppercase", "uppercase", "Test uppercase uppercase"},  //first arg isn't case-sensitive
+}
+
 func TestEmpty(t *testing.T) {
 	const str, minor, want = "", "", ""
 	got := titlecase.TitleCase(str, minor)
@@ -33,10 +49,27 @@ func TestEmpty(t *testing.T) {
 
 func TestWithoutMinor(t *testing.T) {
 	// передайте пустую строку в качестве второго агрумента
-	t.Error("not implemented")
+	const str, minor, want = "test text", "", "Test Text"
+	got := titlecase.TitleCase(str, minor)
+	if got != want {
+		t.Errorf("TitleCase(%v, %v) = %v; want %v", str, minor, got, want)
+	}
 }
 
 func TestWithMinorInFirst(t *testing.T) {
 	// передайте первое слово исходной строки в качестве второго аргумента
-	t.Error("not implemented")
+	const str, minor, want = "test text", "test", "Test Text"
+	got := titlecase.TitleCase(str, minor)
+	if got != want {
+		t.Errorf("TitleCase(%v, %v) = %v; want %v", str, minor, got, want)
+	}
+}
+
+func TestWithDataProvider(t *testing.T) {
+	for _, dataset := range DataProvider {
+		got := titlecase.TitleCase(dataset.Str, dataset.Minor)
+		if got != dataset.Want {
+			t.Errorf("TitleCase(%v, %v) = %v; want %v", dataset.Str, dataset.Minor, got, dataset.Want)
+		}
+	}
 }
