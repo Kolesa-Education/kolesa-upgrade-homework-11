8b193ddd19c5c99ce64fa0a64f7913a6116a4d45
markhabat <msansyzbay@gmail.com>
2022-11-12 03:48:45 +0600

    add: name validation test

--- tests/Validation/PaymentRequestValidatorTest.php
+++ tests/Validation/PaymentRequestValidatorTest.php
@@ -1,1 +1,23 @@
 <?php
+
+declare(strict_types=1);
+
+use App\Validation;
+use App\Validation\PaymentRequestValidator;
+use PHPUnit\Framework\TestCase;
+
+class PaymentRequestValidatorTest extends TestCase
+{
+    public function testNameValidation(): void
+    {
+        $expected = [PaymentRequestValidator::NAME_COUNT_ERROR];
+
+        $generator = new PaymentRequestValidator();
+
+        $actual = $generator->validateName(
+            "Markhabat"
+        );
+
+        $this->assertEquals($expected, $actual);
+    }
+}
