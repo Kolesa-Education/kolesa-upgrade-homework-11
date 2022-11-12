3f05ad23301e71ff37e96a593a80eac177b58310
LexyJexon <kochevoy_aleksey@mail.ru>
2022-11-13 00:03:14 +0600

    Выполняю задание 11, доработал тесты на php

--- src/Validation/PaymentRequestValidator.php
+++ src/Validation/PaymentRequestValidator.php
@@ -12,48 +12,73 @@ class PaymentRequestValidator
         $errs = [];
         $name = $request['name'];
         $cardNumber = $request['cardNumber'];
-        str_replace(" ", "", $cardNumber);
         $expiration = $request['expiration'];
         $cvv = $request['cvv'];
+        $errs[] = $this->validateName($name);
+        $errs[] = $this->validateCardNumber($cardNumber);
+        $errs[] = $this->validateExpiration($expiration);
+        $errs[] = $this->validateCvv($cvv);
 
+        return $errs;
+    }
+
+    public function validateName(string $name): string
+    {
+        $msg = "";
         $fullName = explode(" ", $name);
         if (count($fullName) <2){
             $msg = "Name on card must conteins your full name (name surname)!!!";
-            $errs[] = $msg;
+        } else if ((strlen($fullName[0] ) < 2) || (strlen($fullName[1]) < 2)){
+            $msg = "Name and surname must have at least 2 symbols length!!!";
         }
 
+        return $msg;
+    }
+
+    public function validateCardNumber(string $cardNumber): string
+    {
+        $msg = "";
+        $cardNumber = str_replace(" ", "", $cardNumber);
         if (strlen($cardNumber) < 12){
             $msg = "Card number's length must be 12 symbols!!!";
-            $errs[] = $msg;
         }
 
+        return $msg;
+    }
+
+    public function validateExpiration(string $expiration): array
+    {
+        $msg = [];
         $expireDate = explode("/", $expiration);
         if (count($expireDate) < 2){
-            $msg = "You must enter expiration in format : month/year!!!";
-            $errs[] = $msg;
-        } else if (($expireDate[0] > 12) || ($expireDate < 1)){
-            $msg = "Invalid month number in expiration!!!";
-            $errs[] = $msg;
-        } else if ($expireDate[0] < 10){
-            $monthNumbers = explode("", $expireDate[0]);
+            return ["You must enter expiration in format : month/year!!!"];
+        }
+        if (($expireDate[0] > 12) || ($expireDate[0] < 1)){
+            $msg[] = "Invalid month number in expiration!!!";
+        }
+        if ($expireDate[0] < 10){
+            $monthNumbers = str_split($expireDate[0]);
             if (count($monthNumbers) < 2){
-                $msg = "Invalid month number in expiration, it must starts with 0 if it lower then 10!!!";
-                $errs[] = $msg;
+                $msg[] = "Invalid month number in expiration, it must starts with 0 if it lower then 10!!!";
             }
-        } else if (($expireDate[1] > 25) || ($expireDate < 22)){
-            $msg = "Invalid year number in expiration!!!";
-            $errs[] = $msg;
+        }
+        if (($expireDate[1] > 25) || ($expireDate[1] < 22)){
+            $msg[] = "Invalid year number in expiration!!!";
         }
 
+        return $msg;
+    }
+
+    public function validateCvv(string $cvv): string
+    {
+        $msg = "";
         $cvv = str_split($cvv);
         if ((count($cvv) < 3) || (count($cvv) > 3)){
             $msg = "Invalid cvv length!!!";
-            $errs[] = $msg;
         } else if (($cvv[1] == 0) || ($cvv[2] == 0)){
             $msg = "Invalid cvv!!!";
-            $errs[] = $msg;
         }
 
-        return $errs;
+        return $msg;
     }
 }
--- tests/PaymentRequestValidatorTest.php
+++ tests/PaymentRequestValidatorTest.php
@@ -7,21 +7,54 @@ use PHPUnit\Framework\TestCase;
 
 class PaymentRequestValidatorTest extends TestCase
 {
+
     public function testValidate(): void
     {
-        $request = ['name' => 'Alexey Kochevoy', 'cardNumber' => '544 322 377 437', 'expiration' => '13 22', 'cvv' => '655'];
-
-        $expected = [
-            "You must enter expiration in format : month/year!!!"
-        ];
-
+        $request = [ array('name' => 'A Kochevoy', 'cardNumber' => '544 322 43 127', 'expiration' => '11/12', 'cvv' => '605'),
+            [ "Name and surname must have at least 2 symbols length!!!",
+                "Card number's length must be 12 symbols!!!",
+                ["Invalid year number in expiration!!!"],
+                "Invalid cvv!!!"
+                ]];
         $validator = new PaymentRequestValidator();
+        $actual = $validator->validate($request[0]);
+        $this->assertEquals($request[1], $actual);
+    }
 
-        $actual = $validator->validate($request);
+    public function testValidateName(): void
+    {
+        $request = [ array('name' => 'A Kochevoy', 'cardNumber' => '544 322 743 127', 'expiration' => '11/22', 'cvv' => '615'),
+            [ "Name and surname must have at least 2 symbols length!!!"]];
+        $validator = new PaymentRequestValidator();
+        $actual = $validator->validateName($request[0]['name']);
+        $this->assertEquals($request[1][0], $actual);
+    }
 
-        print_r($expected);
-        print_r($actual);
+    public function testValidateCardNumber(): void
+    {
+        $request = [ array('name' => 'ALexey Kochevoy', 'cardNumber' => '544 322 743 12', 'expiration' => '11/22', 'cvv' => '615'),
+            [ "Card number's length must be 12 symbols!!!"]];
+        $validator = new PaymentRequestValidator();
+        $actual = $validator->validateCardNumber($request[0]['cardNumber']);
+        $this->assertEquals($request[1][0], $actual);
+    }
 
-        $this->assertEquals($expected, $actual);
+    public function testValidateExpiration(): void
+    {
+        $request = [ array('name' => 'ALexey Kochevoy', 'cardNumber' => '544 322 743 123', 'expiration' => '1/12', 'cvv' => '615'),
+            [ "Invalid month number in expiration, it must starts with 0 if it lower then 10!!!",
+                "Invalid year number in expiration!!!"]];
+        $validator = new PaymentRequestValidator();
+        $actual = $validator->validateExpiration($request[0]['expiration']);
+        $this->assertEquals($request[1], $actual);
+    }
+
+    public function testValidateCvv(): void
+    {
+        $request = [ array('name' => 'ALexey Kochevoy', 'cardNumber' => '544 322 743 123', 'expiration' => '11/22', 'cvv' => '15'),
+            ["Invalid cvv length!!!"]];
+        $validator = new PaymentRequestValidator();
+        $actual = $validator->validateCvv($request[0]['cvv']);
+        $this->assertEquals($request[1][0], $actual);
     }
 }
