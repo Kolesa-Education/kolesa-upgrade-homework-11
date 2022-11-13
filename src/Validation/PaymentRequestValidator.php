ab47272259a7f58ed58932b6ef00fc22663cdbf6
Zharkyn <resar3ct@gmail.com>
2022-11-13 23:35:08 +0600

    Задачка тест Пыхыпы

--- src/Validation/PaymentRequestValidator.php
+++ src/Validation/PaymentRequestValidator.php
@@ -9,5 +9,71 @@ class PaymentRequestValidator
     public function validate(array $request): array
     {
         //  здесь должен быть код валидации запроса
+        $nameErr = $this->valName($request['name']);
+        $cardNumErr = $this->valCardnum($request['cardNumber']);
+        $expirErr = $this->valExpir($request['expiration']);
+        $cvvErr = $this->valCvv($request['cvv']);
+        $errors[] = array_merge($nameErr, $cardNumErr, $expirErr, $cvvErr);
+
+        return $errors;
+    }
+
+    public function valName(string $name) : array
+    {
+        $errors = [];
+        if (count($name) != 2){
+            $errors[] = "минимальная длина слова - 2 символа";
+        }
+
+        return $errors;
+    }
+
+    public function valCardnum(string $cardNumber) : array 
+    {
+        $errors = [];
+        if ($cardNumber != 12){
+            $errors[] = "cardNumber должен состоять из 12 цифр";  
+        }
+        if (!ctype_digit($cardNumber)) {
+            $errors[] = "ты не ввел число"; 
+        }
+        return $errors;
+    }
+
+    public function valExpir(string $expiration) : array
+    {
+        $errors = [];
+        $asd = explode( "/" , $expiration);
+        if (count($asd) != 2){
+            $errors[] = "строка формата {число}{число}/{число}{число}";
+        }
+        $parsedInt1 = intval($asd[0]);
+        $parsedInt2 = intval($asd[2]);
+        if ($parsedInt1 <= 12 && $parsedInt2 <= 12){
+            $errors[] = "максимальное значение для первых пар чисел - 12";
+        }
+        $parsedInt3 = intval($asd[1]);
+        $parsedInt4 = intval($asd[3]);
+        if ($parsedInt3 <= 25 && $parsedInt4 <= 25){
+            $errors[] = "для вторых - 25";
+        }
+        if ($parsedInt3 < 22 && $parsedInt4 < 22){
+            $errors[] = "для вторых - 22";
+        }
+
+        return $errors;
+        //поидее на фронте можно было бы выборку даты поставить и не пришлось бы заморачиватся
+    }
+ 
+    public function valCvv(string $cvv) : array
+    {
+        $errors = [];
+        if (!ctype_digit($cvv)) {
+            $errors[] = "ты не ввел число"; 
+        }
+        if (count($cvv) != 3){
+            $errors[] = "cvv должен состоять из 3 цифр";
+        }
+        return $errors;
     }
 }
