21530311a0e170a550614efe36d381c59d42e663
salawhaaat <salauat.kakimzhanov@nu.edu.kz>
2022-11-13 13:38:58 +0600

    add addition provider

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -6,10 +6,30 @@ use PHPUnit\Framework\TestCase;
 use App\Validation\PaymentRequestValidator;
 
 class ValidatorTest extends TestCase{
-
-    public function testValidation($data, $expected):void{
+   /**
+     * @dataProvider additionProvider
+     */
+    public function testValidate($data, $expected)
+    {
         $validator = new PaymentRequestValidator();
         $actual = $validator->validate($data);
         $this->assertEquals($expected, $actual);
+    }
+
+    public function additionProvider(): array
+    {
+        return
+        [
+            "Valid information" =>
+            [
+                [
+                    "name" => "Хамбар Дусалиев",
+                    "cardNumber" => str_repeat("0", 12),
+                    "expiration" => "01/25",
+                    "cvv" => "023"
+                ],
+                []
+            ]
+        ];
     }
 }
